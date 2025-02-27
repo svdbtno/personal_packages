39966f075f7836d6adff9ec152e3ad50657cb4f8
Stefan van den Berg <stefan.vandenberg@tno.nl>
2025-02-27 09:45:44 +0100

    replaced repo_name

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  wildfly = pkgs.callPackage ./pkgs/wildfly { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
